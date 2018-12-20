706848f6ba3674c71a5510a35a739bfd22998a58
naxisinc <33284023+naxisinc@users.noreply.github.com>
2018-12-20 13:46:01 -0500

    Preparing plate

--- client/src/app/components/components.module.ts
+++ client/src/app/components/components.module.ts
@@ -1,15 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { MaterialModule } from '../material';
 import { FooterComponent } from './footer/footer.component';
 import { MainNavComponent } from './main-nav/main-nav.component';
-import { RouterModule } from '@angular/router';
+import { ChipsComponent } from './chips/chips.component';
 
-const COMPONENTS = [FooterComponent, MainNavComponent];
+const COMPONENTS = [FooterComponent, MainNavComponent, ChipsComponent];
 
 @NgModule({
-  declarations: [COMPONENTS],
-  imports: [CommonModule, MaterialModule, RouterModule],
+  declarations: [COMPONENTS, ChipsComponent],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
   exports: [COMPONENTS]
 })
 export class ComponentsModule {}
