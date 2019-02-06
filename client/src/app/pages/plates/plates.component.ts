113fc8001c462dd3b358c733cc6adde87842b83f
naxisinc <33284023+naxisinc@users.noreply.github.com>
2019-02-05 22:03:33 -0500

    Preparing for responsive plate info

--- client/src/app/pages/plates/plates.component.ts
+++ client/src/app/pages/plates/plates.component.ts
@@ -1,39 +1,40 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
+import { Component, OnInit, ViewChild } from '@angular/core';
 import {
   FormControl,
   FormGroup,
   FormBuilder,
   Validators,
   FormArray
-} from "@angular/forms";
-import { MatPaginator, PageEvent } from "@angular/material";
-import { PlatesService } from "../../services/plates.service";
-import { SizesService } from "../../services/sizes.service";
-import { CategoriesService } from "../../services/categories.service";
-import { MyErrorStateMatcher } from "../../services/validator.service";
+} from '@angular/forms';
+import { MatPaginator, PageEvent } from '@angular/material';
+import { PlatesService } from '../../services/plates.service';
+import { SizesService } from '../../services/sizes.service';
+import { CategoriesService } from '../../services/categories.service';
+import { MyErrorStateMatcher } from '../../services/validator.service';
 
 @Component({
-  selector: "app-plates",
-  templateUrl: "./plates.component.html",
-  styleUrls: ["./plates.component.scss"]
+  selector: 'app-plates',
+  templateUrl: './plates.component.html',
+  styleUrls: ['./plates.component.scss']
 })
 export class PlatesComponent implements OnInit {
   searchKey: string;
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  imgPath: string = "http://localhost:3000/images/";
+  imgPath: string = 'http://localhost:3000/images/';
   listData: any; // show the requested array
   listDataCopy: any; // keep the original array
   isSelected: boolean = false;
   selected: Object;
-  @ViewChild("fileInput") fileInput;
+  @ViewChild('fileInput') fileInput;
   // Save the array of ingredients coming from child component
   ingredientsId = [];
   sizes: any;
   // Set the Ingredients in the child component
   setIngredients = [];
-  // Toolbar breakpoint
-  breakpoint: any;
+  // Breakpoins
+  breakpointToolbar: number;
+  breakpointContent: number;
 
   // Reactive Form and Matcher
   parentForm: FormGroup;
@@ -71,7 +72,7 @@ export class PlatesComponent implements OnInit {
       res => {
         this.sortbyGroups = [
           {
-            name: "Categories", // Group name
+            name: 'Categories', // Group name
             categories: res
           }
         ];
@@ -83,11 +84,11 @@ export class PlatesComponent implements OnInit {
 
     this.parentForm = this.fb.group({
       description: [
-        "",
+        '',
         [Validators.required, Validators.minLength(3), Validators.maxLength(50)]
       ],
-      file: ["", [Validators.required]],
-      category: ["", [Validators.required]],
+      file: ['', [Validators.required]],
+      category: ['', [Validators.required]],
       items: this.fb.array([])
     });
   }
@@ -97,12 +98,12 @@ export class PlatesComponent implements OnInit {
       _size: id,
       description: description,
       price: [
-        "",
-        [Validators.required, Validators.pattern("^[0-9]*.[0-9]{2}$")]
+        '',
+        [Validators.required, Validators.pattern('^[0-9]*.[0-9]{2}$')]
       ],
-      calories: ["", [Validators.required, Validators.pattern("^[0-9]+$")]],
-      totalfat: ["", [Validators.required, Validators.pattern("^[0-9]+$")]],
-      totalcarbs: ["", [Validators.required, Validators.pattern("^[0-9]+$")]]
+      calories: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      totalfat: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+      totalcarbs: ['', [Validators.required, Validators.pattern('^[0-9]+$')]]
     });
   }
 
@@ -110,12 +111,14 @@ export class PlatesComponent implements OnInit {
     setTimeout(() => {
       // This's necessary cause sizes is a Observable result
       this.sizes.forEach(size => {
-        this.items = this.parentForm.get("items") as FormArray;
+        this.items = this.parentForm.get('items') as FormArray;
         this.items.push(this.createItem(size._id, size.description));
       });
     }, 500);
+
     // Getting plates
     this.getPlates();
+
     // Formatting the toolbar
     this.onResize(window);
   }
@@ -139,7 +142,7 @@ export class PlatesComponent implements OnInit {
         this.paginator.firstPage();
       },
       err => {
-        console.log("Something is wrong");
+        console.log('Something is wrong');
       }
     );
   }
@@ -157,24 +160,25 @@ export class PlatesComponent implements OnInit {
   onResize(event) {
     let windowSize = event.innerWidth;
     if (windowSize <= 375) {
-      this.breakpoint = this.filtercolspan = 1;
+      this.breakpointToolbar = this.filtercolspan = 1;
     } else if (windowSize > 375 && windowSize <= 1024) {
-      this.breakpoint = 2;
+      this.breakpointToolbar = 2;
       this.filtercolspan = 1;
     } else {
-      this.breakpoint = 3;
+      this.breakpointToolbar = 3;
       this.filtercolspan = 2;
     }
+    this.breakpointContent = windowSize <= 375 ? 1 : 3;
   }
 
   sortby(filterId) {
-    this.searchKey = ""; // limpiando la busqueda
+    this.searchKey = ''; // limpiando la busqueda
     if (filterId.length !== 24) {
       this.listData = this.listDataCopy.sort((val1, val2) => {
-        if (filterId === "rating") {
+        if (filterId === 'rating') {
           return val2.averagerate - val1.averagerate; // descending
           // return val1.averagerate - val2.averagerate; // ascending
-        } else if (filterId === "l2h" || filterId === "h2l") {
+        } else if (filterId === 'l2h' || filterId === 'h2l') {
           let dishes = this.listDataCopy;
           for (let i = 0; i < dishes.length; i++) {
             let avgPriceSum: number = 0;
@@ -183,11 +187,11 @@ export class PlatesComponent implements OnInit {
               avgPriceSum += JSON.parse(details[j].price);
             }
             avgPriceSum = JSON.parse(avgPriceSum.toFixed(2));
-            dishes[i]["avgPrice"] = JSON.parse(
+            dishes[i]['avgPrice'] = JSON.parse(
               (avgPriceSum / details.length).toFixed(2)
             );
           }
-          if (filterId === "l2h") {
+          if (filterId === 'l2h') {
             return val1.avgPrice - val2.avgPrice; // ascending
           } else {
             return val2.avgPrice - val1.avgPrice; // descending
@@ -210,17 +214,17 @@ export class PlatesComponent implements OnInit {
   }
 
   onSearchClear() {
-    this.searchKey = "";
+    this.searchKey = '';
     this.listData = this.listDataCopy;
   }
 
   clearForm() {
     this.parentForm.reset();
     this.sizes.forEach((size, index) => {
-      this.parentForm.controls.items["controls"][
+      this.parentForm.controls.items['controls'][
         index
       ].controls.description.patchValue(size.description);
-      this.parentForm.controls.items["controls"][
+      this.parentForm.controls.items['controls'][
         index
       ].controls._size.patchValue(size._id);
     });
@@ -236,7 +240,7 @@ export class PlatesComponent implements OnInit {
       items: dish.details
     });
     // Ingredients
-    if (!this.isSelected || this.selected["_id"] !== dish._id) {
+    if (!this.isSelected || this.selected['_id'] !== dish._id) {
       this.setIngredients = [];
       dish._ingredients.forEach(ingredient => {
         this.setIngredients.push(ingredient.description);
@@ -250,11 +254,11 @@ export class PlatesComponent implements OnInit {
 
   getFormValues() {
     let sizeDetailsArray = [];
-    this.parentForm.get("items")["controls"].forEach(size => {
+    this.parentForm.get('items')['controls'].forEach(size => {
       sizeDetailsArray.push(size.value);
     });
     let obj = {
-      id: this.isSelected ? this.selected["_id"] : "undefined",
+      id: this.isSelected ? this.selected['_id'] : 'undefined',
       description: this.parentForm.controls.description.value,
       file: this.fileInput.nativeElement.files[0],
       _ingredients: this.ingredientsId,
@@ -270,7 +274,7 @@ export class PlatesComponent implements OnInit {
       res => {
         this.getPlates();
         this.clearForm();
-        this.searchKey = "";
+        this.searchKey = '';
       },
       err => {
         //
@@ -285,23 +289,23 @@ export class PlatesComponent implements OnInit {
         this.getPlates();
         this.clearForm();
         this.isSelected = false;
-        this.searchKey = "";
+        this.searchKey = '';
       },
       error => {
-        console.log("Something is wrong");
+        console.log('Something is wrong');
       }
     );
   }
 
   delete() {
-    this.plateService.deletePlate(this.selected["_id"]).subscribe(
+    this.plateService.deletePlate(this.selected['_id']).subscribe(
       succ => {
         this.getPlates();
         this.clearForm();
         this.isSelected = false;
       },
       error => {
-        console.log("Something is wrong");
+        console.log('Something is wrong');
       }
     );
   }
