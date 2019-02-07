c562863affaba3c92afd0c4a8d7b5264b1adda51
Your Name <you@example.com>
2019-02-07 14:55:34 -0500

    angular-star-rating

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { MaterialModule } from './material';
-import { PagesModule } from './pages/pages.module';
-import { ComponentsModule } from './components/components.module';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { MaterialModule } from "./material";
+import { PagesModule } from "./pages/pages.module";
+import { ComponentsModule } from "./components/components.module";
+import { PageNotFoundComponent } from "./pages/page-not-found/page-not-found.component";
+// import { StarRatingModule } from "angular-star-rating";
 
 @NgModule({
   declarations: [AppComponent, PageNotFoundComponent],
@@ -17,6 +18,7 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     MaterialModule,
     PagesModule,
     ComponentsModule,
+    // StarRatingModule.forRoot(),
     AppRoutingModule // always the last element in the array
   ],
   providers: [],
--- client/src/app/pages/pages.module.ts
+++ client/src/app/pages/pages.module.ts
@@ -1,21 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { StarRatingModule } from "angular-star-rating";
 
-import { PagesRoutingModule } from './pages-routing.module';
-import { MaterialModule } from '../material';
-import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './users/login/login.component';
-import { UsersComponent } from './users/users.component';
-import { SizesComponent } from './sizes/sizes.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { IngredientsComponent } from './ingredients/ingredients.component';
-import { PlatesComponent } from './plates/plates.component';
-import { CommentsComponent } from './comments/comments.component';
-import { ChangePassComponent } from './users/change-pass/change-pass.component';
-import { RecoverPassComponent } from './users/recover-pass/recover-pass.component';
-import { ComponentsModule } from '../components/components.module';
+import { PagesRoutingModule } from "./pages-routing.module";
+import { MaterialModule } from "../material";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./users/login/login.component";
+import { UsersComponent } from "./users/users.component";
+import { SizesComponent } from "./sizes/sizes.component";
+import { CategoriesComponent } from "./categories/categories.component";
+import { IngredientsComponent } from "./ingredients/ingredients.component";
+import { PlatesComponent } from "./plates/plates.component";
+import { CommentsComponent } from "./comments/comments.component";
+import { ChangePassComponent } from "./users/change-pass/change-pass.component";
+import { RecoverPassComponent } from "./users/recover-pass/recover-pass.component";
+import { ComponentsModule } from "../components/components.module";
 
 @NgModule({
   declarations: [
@@ -37,7 +38,8 @@ import { ComponentsModule } from '../components/components.module';
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    ComponentsModule
+    ComponentsModule,
+    StarRatingModule.forRoot()
   ]
 })
 export class PagesModule {}
