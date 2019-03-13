7ae7eb03a4178c54f25858cdcabb8a82421fa78b
naxisinc <33284023+naxisinc@users.noreply.github.com>
2019-03-12 22:16:00 -0400

    Start to user views

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "src/app/services/auth.service";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent implements OnInit {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  onLogoutClick() {
+    this.authService.logout().subscribe(
+      res => {
+        this.router.navigate(["home"]);
+        localStorage.clear();
+      },
+      err => console.log(err)
+    );
   }
-
 }
